4910420cd07acffe45dda2de1cd77ac632ff7a9c
Esther Freixo <freixo.esther@gmail.com>
2024-06-18 08:23:14 -0300

    first commit

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -8,9 +8,9 @@ class App {
 
   constructor() {
     this.app = express();
-    
-    this.config();
 
+    this.config();
+  
     // Não remover essa rota
     this.app.get('/', (req, res) => res.json({ ok: true }));
 
